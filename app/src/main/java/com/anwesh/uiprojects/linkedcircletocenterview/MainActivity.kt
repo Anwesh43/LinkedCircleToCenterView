8ea5a2877f6437bb35bafd9778cc4d4fde9d7159
Anwesh43 <anweshthecool0@gmail.com>
2018-07-29 01:07:44 +0530

    adding CircleToCenterView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcircletocenterview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcircletocenterview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcircletocenterview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.circletocenterview.CircleToCenterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleToCenterView.create(this)
     }
 }
