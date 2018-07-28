3e64235e17ce0e45a05205a0f4f5fa8061ba4f82
Anwesh43 <anweshthecool0@gmail.com>
2018-07-29 00:47:57 +0530

    adding drawCTCNode extension method

--- circletocenterview/src/main/java/com/anwesh/uiprojects/circletocenterview/CircleToCenterView.kt
+++ circletocenterview/src/main/java/com/anwesh/uiprojects/circletocenterview/CircleToCenterView.kt
@@ -9,8 +9,32 @@ import android.content.Context
 import android.view.MotionEvent
 import android.graphics.Paint
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
 
 val nodes : Int = 5
+
+fun Canvas.drawCTCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / nodes
+    val r : Float = gap * 0.4f
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.color = Color.parseColor("#42A5F5")
+    save()
+    translate(i * gap + gap/2, h/2)
+    for (i in 0..3) {
+        save()
+        rotate(90f * i)
+        drawArc(RectF(-r, -r, r, r), 90f * sc1, 90f * (1 - sc1), false, paint)
+        drawLine(r - r * sc1, 0f, r - r * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+}
 
 class CircleToCenterView (ctx : Context) : View(ctx) {
 
@@ -75,6 +99,5 @@ class CircleToCenterView (ctx : Context) : View(ctx) {
                 view.postInvalidate()
             }
         }
-
     }
 }
