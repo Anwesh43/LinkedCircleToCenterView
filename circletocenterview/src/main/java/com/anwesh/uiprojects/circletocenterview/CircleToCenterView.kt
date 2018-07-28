8d63d044fdc55123d0ab0bc425d2a3e65b0f4c17
Anwesh43 <anweshthecool0@gmail.com>
2018-07-29 00:33:43 +0530

    import libraries and declaring number of nodes

---
+++ circletocenterview/src/main/java/com/anwesh/uiprojects/circletocenterview/CircleToCenterView.kt
@@ -0,0 +1,14 @@
+package com.anwesh.uiprojects.circletocenterview
+
+/**
+ * Created by anweshmishra on 29/07/18.
+ */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
+
