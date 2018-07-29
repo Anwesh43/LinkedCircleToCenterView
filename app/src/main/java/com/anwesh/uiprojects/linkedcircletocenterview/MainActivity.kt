de35ce33f34989b9f44acdd086892374393dae65
Anwesh43 <anweshthecool0@gmail.com>
2018-07-29 11:52:00 +0530

    adding AnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcircletocenterview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcircletocenterview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedcircletocenterview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.circletocenterview.CircleToCenterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CircleToCenterView.create(this)
+        val view : CircleToCenterView = CircleToCenterView.create(this)
         fullScreen()
+        view.addAnimationListener({
+            Toast.makeText(this, "${it} animation is complete", Toast.LENGTH_SHORT).show()
+        }, {
+            Toast.makeText(this, "${it} animation is reset", Toast.LENGTH_SHORT).show()
+        })
     }
 }
 
